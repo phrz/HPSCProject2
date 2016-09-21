294e7d1d5ca4fe9e3f7f0c3b62aa9e2aaf86d801
Paul Herz <phrz@me.com>
2016-09-21 18:58:14 -0500

    Initial Commit

---
+++ src/vandermonde.cpp
@@ -0,0 +1,19 @@
+//
+//  vandermonde.cpp
+//  HPSCProject2
+//
+//  Created by Paul Herz on 9/21/16.
+//  Copyright © 2016 Paul Herz. All rights reserved.
+//
+
+#include <iostream>
+
+#include "Matrix.h"
+#include "Vector.h"
+
+int main(int argc, const char * argv[]) {
+	
+	
+	
+	return 0;
+}

e3f7a737f7d3fe060aab197db98f4d2f9a159f25
Paul Herz <phrz@me.com>
2016-09-22 09:04:15 -0500

    Added vandermonde error and residual norms

---
+++ src/println.cpp
@@ -0,0 +1,22 @@
+// [CITE] http://coliru.stacked-crooked.com/a/92a50828d6cb6f01
+
+#include <iostream>
+
+template<typename T>
+void println(const T& t) {
+	std::cout << t << std::endl;
+}
+
+template<typename... Args>
+void println(Args... a);
+
+template<typename T, typename... Args>
+void println(const T& t, const Args&... a) {
+	std::cout << t << " ";
+	println(a...);
+}
+
+template<>
+void println() {
+	std::cout << std::endl;
+}
--- src/vandermonde.cpp
+++ src/vandermonde.cpp
@@ -10,10 +10,68 @@
 
 #include "Matrix.h"
 #include "Vector.h"
+#include "println.cpp"
+
+using namespace PH;
+
+Matrix makeVandermonde(Vector v) {
+	// The Vandermonde matrix of order n is an (n,n) matrix is defined as
+	// A(r,c) = v[r] ^ c (assuming zero indexing!)
+	
+	Matrix vandermonde = Matrix(v.size(), v.size());
+	
+	vandermonde.mapElements([&](double& element, Index r, Index c) {
+		element = std::pow(v[r], c);
+	});
+	
+	return vandermonde;
+}
 
 int main(int argc, const char * argv[]) {
 	
+	// Solve a linear system Ax=b with a Vandermonde A of order n
+	// for each n = 5, 9, 17, 33, 65.
+	for(auto n : {5, 9, 17, 33, 65}) {
+		
+		println("n =",n,":");
+		
+		// To generate the matrix, use vector v with an n-size linear span
+		// 0...1 (inclusive), and vector x with n random entries.
+		
+		Vector v = Vector::linSpace(0, 1, n);
+		Vector x = Vector::random(n);
+		
+		Matrix A = makeVandermonde(v);
+		
+		// Set b to be equal to A*x so the exact solution is known.
+		
+		Vector b = A*x;
+		
+		// find x-hat using the linear solver with A and b.
+		
+		try {
+			Vector xHat = Matrix::linearSolve(A, b);
+			
+			// print the 2-norm of the error vector and the residual vector
+			Vector errorVector = Vector(n);
+			errorVector.mapElements([&](double& element, Index i) {
+				element = std::abs( xHat[i] - x[i] );
+			});
+			
+			Vector residualVector = (A * xHat) - b;
+			
+			println("  • Error vector 2-norm:", Vector::norm(errorVector));
+			println("  • Residual vector 2-norm:", Vector::norm(residualVector));
+			
+		} catch(std::runtime_error* e) {
+			println("  • Matrix is singular");
+		}
+		
+		println("-------------------------------");
+		
+	}
 	
+	// output can be piped to file with "binaryfile > outputfile"
 	
 	return 0;
 }
